b3e53788120a35c88f7550e6fd85172d1e9b9c39
Konstantin <grigo.ko2004@gmail.com>
2026-03-19 16:26:16 +0500

    The user service and repository interfaces have been designed

--- lesson_5/tasks/app/Repositories/Contracts/UserRepositoryInterface.php
+++ lesson_5/tasks/app/Repositories/Contracts/UserRepositoryInterface.php
@@ -1,7 +1,14 @@
 <?php
 namespace App\Repositories\Contracts;
 
+use App\Models\User;
+use Illuminate\Database\Eloquent\Collection;
+
 interface UserRepositoryInterface
 {
-
+    public function getUsers(): Collection;
+    public function getUserById(int $id): User;
+    public function createUser(string $name, string $email, string $password): User;
+    public function updateUser(int $id, array $data): bool;
+    public function deleteUser(int $id): bool;
 }
--- lesson_5/tasks/app/Services/Contracts/UserServiceInterface.php
+++ lesson_5/tasks/app/Services/Contracts/UserServiceInterface.php
@@ -1,6 +1,13 @@
 <?php
 namespace App\Services\Contracts;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Collection;
+
 interface UserServiceInterface
 {
-
+    public function getAll(): Collection;
+    public function getById(int $id): User;
+    public function create(array $data): User;
+    public function update(int $id, array $data): bool;
+    public function delete(int $id): bool;
 }
